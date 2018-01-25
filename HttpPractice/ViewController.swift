82eab237f74410ab9aa9765744a3939587646c88
Leo Chou <leo.chou@fuhu.com>
2018-01-25 11:33:31 +0800

    First Commit

---
+++ HttpPractice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HttpPractice
+//
+//  Created by steven.chou on 2018/1/25.
+//  Copyright © 2018年 steven.chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
